fd5bdd847ce1cbd717ea661597be522cf0ef2c7f
Samantha Thorne <150044477+13Thornes@users.noreply.github.com>
2024-08-07 12:46:08 -0230

    struggling with secret

--- index.js
+++ index.js
@@ -1,31 +1,46 @@
+if( process.env.NODE_ENV !== 'production') {
+    require('dotenv').config();
+}
 
 // define constants
 const express = require('express');
-const methodOverride = require('method-override');
 const app = express();
-const PORT = 3000;
-const path = require('path');
-
-// Debug, set and use statements
+const session = require('express-session');
+const PORT = process.env.PORT || 3000;
 global.DEBUG = true;
+
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
-app.use(express.urlencoded({ extended: true, }));
-app.use(methodOverride('_method'));
-app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: false
+}));
+const myEventEmitter = require('./services/logEvents.js');
 
-// get games page
-const animalsRouter = require('./Routes/animalspg')
-app.use('/animals', animalsRouter);
+app.listen(PORT, (err) => {
+    if (err) console.log(err);
+    myEventEmitter.emit('event', 'app.listen', 'SUCCESS', 'http search site successfully started.');
+    console.log(`Simple app running on port ${PORT}.`)
+});
 
+app.get('/', async (req, res) => {
+    myEventEmitter.emit('event', 'app.get', 'INFO', 'landing page (index.ejs) was displayed.');
+    res.render('index', {status: req.session.status});
+});
 
+app.get('/about', async (req, res) => {
+    myEventEmitter.emit('event', 'app.get /about', 'INFO', 'about page (about.ejs) was displayed.');
+    res.render('about', {status: req.session.status});
+});
 
-// Error page
+const searchRouter = require('./routes/search');
+app.use('/search', searchRouter);
+
 app.use((req, res) => {
-    res.status(404).render('404');
+    res.status(404).render('404', {status: req.session.status});
 });
 
-// run the server
-app.listen(PORT, () => {
-    console.log(`App running on port ${PORT}.`)
-});
+
+
