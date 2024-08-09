b584d695d64850b8ef8f9d92bc1a707a37129e18
lpnl3456 <luke.peddle@keyin.com>
2024-08-09 11:27:14 -0230

    Can Choose between mongo and pg

--- Services/pg.animals.dal.js
+++ Services/pg.animals.dal.js
@@ -3,20 +3,27 @@
 const dal = require("./animals_db");
 
 // GET all animals
-var getanimals= function() {
-    if(DEBUG) console.log("animals.pg.dal.getanimals()");
-    return new Promise(function(resolve, reject) {
-      const sql = "SELECT \"animal_id\", \"nickname\", \"common_name\", \"scientific_name\", \"age\", \"date_arrived\", \"last_checkup\" FROM \"animals\" ORDER BY \"animal_id\";"
-      dal.query(sql, [], (err, result) => {
-        if (err) {
-          if(DEBUG) console.log(err);
-          reject(err);
-        } else {
-          resolve(result.rows);
-        }
-      }); 
+var getanimals= function(text) {
+  if(DEBUG) console.log("animals.pg.dal.getanimals()");
+  return new Promise(function(resolve, reject) {
+
+    const sql = `SELECT animal_id, nickname, common_name, scientific_name, age, date_arrived, last_checkup FROM public.animals \
+  WHERE nickname iLIKE '%'||$1||'%' \
+        OR common_name iLIKE '%'||$1||'%' \
+        OR scientific_name iLIKE '%'||$1||'%'`;
+    
+    if(DEBUG) console.log(sql);
+    dal.query(sql, [text], (err, result) => {
+      if (err) {
+        if(DEBUG) console.log(err);
+        reject(err);
+      } else {
+        if(DEBUG) console.log(`Row count: ${result.rowCount}`);
+        resolve(result.rows);
+      }
     }); 
-  };
+  }); 
+};
 
   // GET one animal by its animal_id
   var getanimalbyanimalid = function(id) {
