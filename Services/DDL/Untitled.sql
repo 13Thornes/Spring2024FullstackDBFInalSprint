e5c3a8c340626cf725b7964195f7336df3bd727a
lpnl3456 <luke.peddle@keyin.com>
2024-08-10 16:17:02 -0230

    Updated the sql statement

--- Services/DDL/Untitled.sql
+++ Services/DDL/Untitled.sql
@@ -1,10 +1,11 @@
 CREATE TABLE public."User"
 (
+    id serial not null,
     username character varying(1000) NOT NULL,
     password character varying(2000) NOT NULL,
     email character varying(200),
     uuid uuid NOT NULL,
-    PRIMARY KEY (username)
+    PRIMARY KEY (id)
 );
 
 ALTER TABLE IF EXISTS public."User"
