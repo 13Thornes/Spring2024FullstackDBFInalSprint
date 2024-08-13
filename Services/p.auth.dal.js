540095d3c0f0f95a416594518d14e584a150c61d
Luke Peddle <150044410+lpnl3456@users.noreply.github.com>
2024-08-12 23:08:37 -0230

    fixed bug

--- Services/p.auth.dal.js
+++ Services/p.auth.dal.js
@@ -35,10 +35,10 @@ async function addLogin(name, email, password, uuidv4) {
     }  
   };
 
-  async function deleteUser(username) {
-    let SQL = "DELETE FROM public.\"User\" WHERE \"username\" = $1";
+  async function deleteUser(id) {
+    let SQL = "DELETE FROM public.\"User\" WHERE \"id\" = $1";
     try {
-      let results = await dal.query(SQL, [username]);
+      let results = await dal.query(SQL, [id]);
       return results
     } catch (error) {
       return error
